7c8519612a095d223fa8f776a862cd3033d12fa8
Jason C. Leach <jason.leach@fullboar.ca>
2018-06-07 10:26:25 -0700

    Code doc and refactoring

--- src/libs/sign.js
+++ src/libs/sign.js
@@ -29,23 +29,58 @@ import shortid from 'shortid';
 
 const exec = util.promisify(cp.exec);
 
+/**
+ * Package a signed artifact for delivery into a ZIP.
+ *
+ * @param {string} archiveFilePath The path to the uploaded archive (ZIP)
+ * @param {string} workspace The workspace to use
+ * @returns A `string` containing the path to extracted contents
+ */
+const extractArchiveContents = async (archiveFilePath, workspace) => {
+  const apath = path.join(workspace, shortid.generate());
+
+  await exec(`
+    mkdir -p ${apath} && \
+    unzip -q ${archiveFilePath} -d ${apath}
+  `);
+
+  return apath;
+};
 
 /**
- * Helper function to wrap express rountes to handle rejected promises
+ * Package a signed artifact for delivery into a ZIP.
  *
- * @param {Function} fn The `next()` function to call
+ * @param {string} apath The path to the signed artifacts
+ * @param {string} items The items to be included in the archive
+ * @returns A `string` containing the path to the newly minted ZIP archive
+ */
+const packageForDelivery = async (apath, items) => {
+  const fileName = `${shortid.generate()}.zip`;
+  const zipResult = await exec(`
+    cd ${apath} && \
+    zip -6rq -n 'ipa' ${fileName} ${items.map(i => path.basename(i)).join(' ')} && \
+    echo 'OK' || echo 'FAIL'
+  `);
+
+  if (zipResult.stderr !== '' || zipResult.stdout.includes('FAIL')) {
+    throw new Error('Unable to create delivery package');
+  }
+
+  return Promise.resolve(path.join(apath, fileName));
+};
+
+/**
+ * Sign an xcode xcarchive file.
+ *
+ * @param {string} archiveFilePath The path to the xcarchive file
+ * @param {string} [workspace='/tmp/'] The workspace to use
+ * @returns A `string` containing the path to the newly minted archive
  */
 // eslint-disable-next-line import/prefer-default-export
 export const signxcarchive = async (archiveFilePath, workspace = '/tmp/') => {
   try {
-    const apath = path.join(workspace, shortid.generate());
     const outputDir = 'signed';
-
-    await exec(`
-      mkdir -p ${apath} && \
-      unzip -q ${archiveFilePath} -d ${apath}
-    `);
-
+    const apath = await extractArchiveContents(archiveFilePath, workspace);
     const findResult = await exec(`find ${apath} -iname '*.xcarchive'`);
     if (findResult.stderr !== '') {
       throw new Error('Unable to find xcarchive(s) in package');
@@ -76,21 +111,24 @@ export const signxcarchive = async (archiveFilePath, workspace = '/tmp/') => {
       }
     });
 
-    const fileName = `${shortid.generate()}.zip`;
-    const zipResult = await exec(`
-      cd ${path.join(apath, outputDir)} && \
-      zip -6rq -n 'ipa' ${fileName} ${items.map(i => path.basename(i)).join(' ')} && \
-      echo 'OK' || echo 'FAIL'
-    `);
+    const deliveryFile = packageForDelivery(path.join(apath, outputDir), items);
 
-    if (zipResult.stderr !== '' || zipResult.stdout.includes('FAIL')) {
-      throw new Error('Unable to create delivery package');
-    }
-
-    return Promise.resolve(path.join(apath, outputDir, fileName));
+    return deliveryFile;
   } catch (error) {
     console.log(error.message);
   }
 
   return Promise.reject();
+};
+
+/**
+ * Sign an xcode ipa file.
+ *
+ * @param {string} archiveFilePath The path to the ipa file
+ * @param {string} [workspace='/tmp/'] The workspace to use
+ * @returns A `string` containing the path to the newly minted archive
+ */
+// eslint-disable-next-line no-unused-vars
+export const signipaarchive = async (archiveFilePath, workspace = '/tmp/') => {
+  throw new Error('Not Implemented');
 };
