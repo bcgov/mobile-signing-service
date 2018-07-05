86e34e9bee51ba7efa21f193aa0dc90dfcdd702a
Jason C. Leach <jason.leach@fullboar.ca>
2018-07-05 10:23:38 -0700

    Fix cert path in KC make script

--- scripts/mkc.sh
+++ scripts/mkc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 KC_NAME=cicd
+CERT_PATH=~/Desktop/Certificates.p12
 
 security create-keychain -p $1 $KC_NAME.keychain
-security import ~/Desktop/Certificates.p12 -k $KC_NAME.keychain -P $2 -T /usr/bin/codesign
+security import $CERT_PATH -k $KC_NAME.keychain -P $2 -T /usr/bin/codesign
 security list-keychains -d user -s $KC_NAME.keychain
 security unlock-keychain -p $1 $KC_NAME.keychain
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME.keychain
