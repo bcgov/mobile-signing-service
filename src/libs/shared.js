3e6a2ac0b5e7ad492e050544ea85e73296ebea8a
ShellyXueHan <shelly@freshworks.io>
2018-07-27 11:30:42 -0700

    fix minio para mismatch

--- src/libs/shared.js
+++ src/libs/shared.js
@@ -28,7 +28,7 @@ const gs = Object.getOwnPropertySymbols(global);
 
 if (!(gs.indexOf(key) > -1)) {
   global[key] = new minio.Client({
-    endPoint: config.get('minio:host'),
+    endPoint: config.get('minio:endPoint'),
     port: config.get('minio:port'),
     secure: config.get('minio:secure'),
     accessKey: config.get('minio:accessKey'),
