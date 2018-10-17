ca46f3f4b668a61fcf4cd450504300f36cfecbe3
ShellyXueHan <shellshell456@gmail.com>
2018-10-17 09:47:20 -0700

    add a sample aw project groups data

--- src/libs/db/seeds/project_and_aw_org_group.js
+++ src/libs/db/seeds/project_and_aw_org_group.js
@@ -2,30 +2,30 @@
 
 'use strict';
 
-const ref_aw_table = 'ref_aw_deployment_group';
-const ref_project_table = 'project';
+const refAwTable = 'ref_aw_deployment_group';
+const refProjectTable = 'project';
 const table = 'job';
 
-exports.seed = async (knex) => {
+exports.seed = async knex => {
   // this is fake data:
   const awData = [
     {
       aw_code: '848',
       aw_group_name: 'Citizens Service',
       aw_group_id: 'CITZ',
-    }
+    },
   ];
 
   const projectData = [
     {
       project_name: 'testDeployment',
       aw_group_id: 'CITZ',
-    }
+    },
   ];
 
   await knex(table).delete();
-  await knex(ref_project_table).delete();
-  await knex(ref_aw_table).delete();
-  await knex(ref_aw_table).insert(awData);
-  await knex(ref_project_table).insert(projectData);
+  await knex(refProjectTable).delete();
+  await knex(refAwTable).delete();
+  await knex(refAwTable).insert(awData);
+  await knex(refProjectTable).insert(projectData);
 };
