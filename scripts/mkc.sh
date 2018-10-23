02a2c5d149d28f642add7dd64fea6c51b73061c7
Jason C. Leach <jason.leach@fullboar.ca>
2018-10-23 09:47:48 -0700

    Fix keychian mgmt script

--- scripts/mkc.sh
+++ scripts/mkc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KC_NAME=cicd
+KC_NAME=cicd.keychain
 CERT_PATH=/Users/xcode/certificates.p12
 
 echo "Keychain Starting"
@@ -9,25 +9,26 @@ echo "Keychain Starting"
 # Code 1 = Locked
 # Code 36 = Locked
 # Code 50 = Not Found
-/usr/bin/security show-keychain-info $KC_NAME.keychain
+/usr/bin/security show-keychain-info $KC_NAME 2>/dev/null
+rv=$?
 
-if [ $? -eq 0 ]; then
-  /usr/bin/security list-keychains -d user -s $KC_NAME.keychain
+if [ $rv -eq 0 ]; then
+  /usr/bin/security list-keychains -d user -s $KC_NAME
   echo "Keychain ready"
-elif [ $? -eq 50 ]; then
+elif [ $rv -eq 50 ]; then
   echo "Building keychian"
-  /usr/bin/security create-keychain -p $1 $KC_NAME.keychain
-  /usr/bin/security unlock-keychain -p $1 $KC_NAME.keychain
-  /usr/bin/security list-keychains -d user -s $KC_NAME.keychain
-  /usr/bin/security import $CERT_PATH -k $KC_NAME.keychain -P $1 -T /usr/bin/codesign
-  /usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME.keychain
-  /usr/bin/security set-keychain-settings $KC_NAME.keychain
-elif [ $? -eq 1 ]; then
+  /usr/bin/security create-keychain -p $1 $KC_NAME
+  /usr/bin/security unlock-keychain -p $1 $KC_NAME
+  /usr/bin/security list-keychains -d user -s $KC_NAME
+  /usr/bin/security import $CERT_PATH -k $KC_NAME -P $1 -T /usr/bin/codesign
+  /usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME
+  /usr/bin/security set-keychain-settings $KC_NAME
+elif [ $rv -eq 1 ] || [ $rv -eq 36 ] ; then
   echo "Unlocking keychain"
-  /usr/bin/security unlock-keychain -p $1 cicd.keychain
-  /usr/bin/security show-keychain-info $KC_NAME.keychain
+  /usr/bin/security unlock-keychain -p $1 $KC_NAME
+  /usr/bin/security show-keychain-info $KC_NAME
 else
-  echo "Unable to process keychain, error = $?"
+  echo "Unable to process keychain, error = $rv"
   exit 1
 fi
 
