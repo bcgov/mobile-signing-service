f6a1364f8be6f0111ef7e6cd3970d4601c09edbb
Jason C. Leach <jason.leach@fullboar.ca>
2021-03-19 14:42:00 -0700

    Logging

--- api/src/router/routes/sign.js
+++ api/src/router/routes/sign.js
@@ -131,6 +131,7 @@ router.post(
         body,
         json: true,
       };
+      console.log(JSON.stringify(options));
       const status = await request(options);
 
       if (status !== 'OK') {
@@ -156,7 +157,7 @@ router.post(
       logger.info(`5/5: Sent response 202 to client.`);
     } catch (err) {
       const message = 'Unable to create signing job';
-      logger.error(`${error} = ${err}`);
+      logger.error(`error = ${err}`);
       logger.error(`${message}, err = ${err.message}`);
 
       if (err.code) {
